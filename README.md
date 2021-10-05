ae7d78e8f0dcb3dc3e54ab66eaaf8afe3cbe3c50
Hagiwara Tsutomu <tenkao@gmail.com>
2021-10-06 00:11:05 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+- [Next.js最速セットアップ](https://zenn.dev/a_da_chi/articles/181ea4ccc39580) をもとにした Next.js プロジェクトのテンプレート
+- 作業ログ的なのはこちら
+  - [Next.js プロジェクトのセットアップ](https://zenn.dev/tenkao/scraps/ca17854714bf68)
---
+++ src/__tests__/utils/index.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { render } from '@testing-library/react'
+import React from 'react'
+// import theme from 'src/theme'
+// 存在せずエラーになるのでとりあえずコメントアウト
+
+const AllTheProviders = ({ children }: { children: JSX.Element }) => {
+  return <ChakraProvider>{children}</ChakraProvider>
+}
+
+const customRender = (ui: JSX.Element, options?: any) =>
+  render(ui, { wrapper: AllTheProviders, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
